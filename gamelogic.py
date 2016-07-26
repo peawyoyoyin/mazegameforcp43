a688e3da25ca83bb7758e54556476fae7a9f25c2
peawyoyoyin <peawyoyoyin@gmail.com>
2016-07-26 23:21:39 +0700

    player update

--- gamelogic.py
+++ gamelogic.py
@@ -5,6 +5,8 @@ TWO_WALL_CHANCE = 5.00
 THREE_WALL_CHANCE = 0.00
 FOUR_WALL_CHANCE = 0.00
 
+PLAYER_STARTING_POSITION = (0,0)
+
 BOARD_SIZE = 10
 TOTAL_POINTS = 20
 
@@ -22,6 +24,44 @@ class Gamelogic():
 
         #initialize board size
         self.board = BoardHandler.generateBoard(BOARD_SIZE)
+
+        #initialize player
+        self.playerx,self.playery = self.playerpos = PLAYER_STARTING_POSITION
+        
+
+    def isplayer(self,x,y):
+        if (x,y) == (self.playerx,self.playery):
+            return True
+        return False
+
+    def turn(self,turnstring):
+        if turnstring == "up":
+            #check if up is a valid move
+            #"up" : y-=1
+            if playery == 0 or board[playery][playerx].wallup or board[playery-1][playerx].walldown:
+                #invalid move
+                print("invalid move")
+
+            else:
+                playery-=1
+        elif turnstring == "down":
+            #check if down is a valid move
+            #"down" : y+=1
+            if playery == BOARD_SIZE or board[playery][playerx].walldown or board[playery+1][playerx].wallup:
+                #invalid move
+                print("invalid move")
+    
+        elif turnstring == "left":
+            #check if left is a valid move
+            pass
+    
+        elif turnstring == "right":
+            #check if right is a valid move
+            pass
+    
+        elif turnstring == "idle":
+            #stay idle
+            pass
 
 class BoardHandler():
     def printBoard(board):
@@ -43,6 +83,7 @@ class BoardHandler():
         #generate points
         temp = coordlist.copy()
         temp.remove((dim-1,dim-1))
+        temp.remove(PLAYER_STARTING_POSITION)
 
 
         temp2 = random.sample(coordlist, TOTAL_POINTS)
