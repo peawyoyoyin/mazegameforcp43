ab74ccce85552cf76de4085239f31b9afabc482f
peawyoyoyin <peawyoyoyin@gmail.com>
2016-07-26 18:59:10 +0700

    game control

--- mazegame.py
+++ mazegame.py
@@ -2,12 +2,35 @@
 
 import pygame
 
-pygame.init()
+class Game():
+    def __init__(self):
+        pygame.init()
 
-DISPLAY_SIZE = DISPLAY_WIDTH , DISPLAY_HEIGHT = 800,600
-sc = pygame.display.set_mode(DISPLAY_SIZE)
-pygame.display.set_caption("CP43 maze game")
+        DISPLAY_SIZE = DISPLAY_WIDTH , DISPLAY_HEIGHT = 800,600
+        self.screen = pygame.display.set_mode(DISPLAY_SIZE)
+        pygame.display.set_caption("CP43 maze game")
 
-for event in pygame.event.get():
-    if event.type == pygame.QUIT:
+        self.clock=pygame.time.Clock()
+        self.running = True
+        
+    def update(self):
+        self.clock.tick(60)
+
+        self.screen.fill(0)
+
+        pygame.display.flip()
+
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.quit()
+
+
+    def quit(self):
+        self.running = False
         pygame.quit()
+
+
+maze = Game()
+while maze.running:
+    maze.update()
